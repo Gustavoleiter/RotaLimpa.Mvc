e87740219e27985cdce380a1f8d6198903efe5b8
GUSTAVO LEITE RIBEIRO <gustavu0720@gmail.com>
2023-11-06 18:21:27 -0300

    adição da lista de setores view model

--- RotaLimpa.Mvc/Services/Setores/SetorService.cs
+++ RotaLimpa.Mvc/Services/Setores/SetorService.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using RotaLimpa.Mvc.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RotaLimpa.Mvc.Services.Setores
 {
-    internal class SetorService
+    public class SetorService : Request
     {
+        private readonly Request _request;
+        private const string apiUrlBase = "http://rotalimpabanco.somee.com/rota/Setores";
+
+        public SetorService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<Setor> PostSetorAsync(Setor s)
+        {
+            string urlComplementar = "/Registrar";
+            s.Id = await _request.PostReturnIntAsync(apiUrlBase + urlComplementar, s);
+            return s;
+                
+        }
+
+        public async Task<ObservableCollection<Setor>> GetSetoresAsync()
+        {
+            string urlComplementar = string.Format("{0}", "/GetAll");
+            ObservableCollection<Setor> listaSetores = await
+            _request.GetAsync<ObservableCollection<Setor>>(apiUrlBase + urlComplementar);
+            return listaSetores;
+        }
+
+        public async Task<Setor> GetSetorAsync(int setorId)
+        {
+            string urlComplementar = string.Format("/{0}", setorId);
+            var setor = await _request.GetAsync<Setor>(apiUrlBase + urlComplementar);
+            return setor;
+        }
+
+        public async Task<int> PutSetorAsync(Setor s)
+        {
+            var result = await _request.PutAsync(apiUrlBase, s);
+            return result;
+        }
+
+        public async Task<int> DeleteSetorAsync(int setorId)
+        {
+            string urlComplementar = string.Format("/{0}", setorId);
+            var result = await _request.DeleteAsync(apiUrlBase + urlComplementar);
+            return result;
+        }
+
     }
 }
+
--- RotaLimpa.Mvc/ViewModels/Setor/ListagemSetorViewModel.cs
+++ RotaLimpa.Mvc/ViewModels/Setor/ListagemSetorViewModel.cs
@@ -1,12 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using RotaLimpa.Mvc.Models;
+using RotaLimpa.Mvc.Services.Setores; // Importe o serviço de Setores
+using RotaLimpa.Mvc.ViewModels;
+using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
-namespace RotaLimpa.Mvc.ViewModels.Setor
+namespace AppRpgEtec.ViewModels.Setores
 {
-    internal class SetorViewModel
+    public class ListagemSetorViewModel : BaseViewModel
     {
+        private SetorService setorService;
+
+        public ObservableCollection<Setor> Setores { get; set; }
+
+        public ListagemSetorViewModel()
+        {
+           
+            Setores = new ObservableCollection<Setor>();
+
+            _ = ObterSetores();
+            NovoSetor = new Command(async () => { await ExibirCadastroSetor(); });
+            RemoverSetorCommand = new Command<Setor>(async (Setor setor) => { await RemoverSetor(setor); });
+        }
+
+        public ICommand RemoverSetorCommand { get; }
+        public ICommand NovoSetor { get; }
+
+        public async Task ObterSetores()
+        {
+            try
+            {
+                Setores = await setorService.GetSetoresAsync();
+                OnPropertyChanged(nameof(Setores));
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "ok");
+            }
+        }
+
+        public async Task ExibirCadastroSetor()
+        {
+            try
+            {
+                await Shell.Current.GoToAsync("cadSetorView");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+       
+        public async Task RemoverSetor(Setor setor)
+        {
+            try
+            {
+                if (await Application.Current.MainPage
+                    .DisplayAlert("Confirmação", $"Confirma a remoção do setor {setor.Id}?", "Sim", "Não"))
+                {
+                    await setorService.DeleteSetorAsync(setor.Id);
+
+                    await Application.Current.MainPage.DisplayAlert("Mensagem",
+                        "Setor removido com sucesso!", "Ok");
+                    _ = ObterSetores();
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
     }
 }
+
